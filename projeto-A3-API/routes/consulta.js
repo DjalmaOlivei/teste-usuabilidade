4dc224b55ccfdcc0f1fc7cd56d6f0a77b086a56d
DjalmaOlivei <79875503+DjalmaOlivei@users.noreply.github.com>
2022-11-14 18:36:45 -0300

    commit

---
+++ projeto-A3-API/routes/consulta.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const router = express.Router()
+
+const Consulta = require('../modules/Consulta')
+
+router.use(express.json())
+
+
+router.post('/add',(req, res, next) => {
+    
+    let consulta = new Consulta ({
+        data:   req.body.data,//'2022-11-02T15:00',
+        cpf:   req.body.cpf,//"123.456.789.00",
+        status:   "agendado",
+        crm:   req.body.crm,//"12345678-9/UF",
+        obs: req.body.obs
+    })
+    consulta.save()
+    .then(consulta => {
+        res.json({
+            message : 'consulta created successfully!',
+            consulta
+        })
+    }).catch(err => {
+        res.status(500).json({
+            message : "An error occured"+err
+        }
+        )
+    })}
+)
+
+router.delete("/delete", (req, res, next) => {
+    let consultaID = req.body.consultaID
+
+    Consulta.findOneAndRemove(consultaID)
+    .then(() => {
+       res.json({ 
+        message: "consulta apagada"
+    })
+    })
+    .catch((err) => {
+        res.json({
+            message: "um erro ocorreu"
+        })
+    })
+})
+
+router.post('/update', (req, res, next) => {
+    let consultaID = req.body.consultaID
+
+    let consultaData = {
+        data:   req.body.data,//'2022-11-02T15:00',
+        cpf:   req.body.cpf,//"123.456.789.00",
+        status:   "agendado",
+        crm:   req.body.crm,//"12345678-9/UF",
+        obs: req.body.obs
+    }
+
+    Consulta.findByIdAndUpdate(consultaID, {$set: consultaData})
+    .then(() =>{
+        res.json({
+            message: 'consulta modificada'
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.json({
+            message: 'um erro ocorreu'
+        })
+    })
+
+})
+
+router.get('/list', (req, res, next) => {
+    try{
+        Consulta.find(req.body).then((response) =>{
+            res.status(200).json({response})
+        })
+        
+    }
+    catch(err){
+        res.status(500).send(err)
+    }
+
+})
+
+module.exports = router
+
---
+++ projeto-A3-API/server.js
@@ -0,0 +1,37 @@
+const express =     require("express")
+const mongoose =    require("mongoose")
+const dotenv =      require("dotenv")
+
+dotenv.config()
+
+
+// mongo connection
+console.log(process.env.HOST + "\n")
+mongoose.connect('mongodb://'+process.env.HOST+':27017/testdb',{useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection 
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open', () => {
+    console.log('Database connection established!')
+})
+
+//express server
+const app =  express()
+
+const AuthRoute =   require('./routes/auth')
+const ConsultaRoute =   require('./routes/consulta')
+const Authentication = require("./middleware/authenticate")
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT , () => {
+    console.log("listen on port :"+ PORT)
+})
+
+app.use('/user', AuthRoute)
+app.use('/consulta',Authentication , ConsultaRoute) 
