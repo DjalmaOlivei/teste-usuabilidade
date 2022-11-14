71c87d5603f7ba14d8e13bfef1c7505c0394aba9
DjalmaOlivei <79875503+DjalmaOlivei@users.noreply.github.com>
2022-11-14 19:19:01 -0300

    commit 4

--- projeto-A3-API/server.js
+++ projeto-A3-API/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 // mongo connection
 console.log(process.env.HOST + "\n")
-mongoose.connect('mongodb://'+process.env.HOST+'/testdb',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(`mongodb+srv://${'lucas'}:${'lucas123'}@${'cluster0'}.${'fxzv4gp'}.mongodb.net/?retryWrites=true&w=majority`,{useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection 
 
 db.on('error', (err) => {
