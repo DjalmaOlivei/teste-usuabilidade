70098b71349afcd007a40c9be544456a037da1f7
DjalmaOlivei <79875503+DjalmaOlivei@users.noreply.github.com>
2022-11-15 16:52:50 -0300

    Update consulta.js

--- projeto-A3-API/routes/consulta.js
+++ projeto-A3-API/routes/consulta.js
@@ -74,7 +74,6 @@ router.post('/update', (req, res, next) => {
 router.post('/list', (req, res, next) => {
 
     const requestBody = req.body
-    re
     try{
         Consulta.find(requestBody).then((response) =>{
             res.status(200).json({response})
